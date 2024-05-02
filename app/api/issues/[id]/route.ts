023ef72b29f9632664ccac34021822ba41fc28cd
Jawwad Uddin <m.m.j.uddin12@gmail.com>
2024-05-02 21:03:55 +0100

    Build an API for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -24,3 +24,17 @@ export async function PATCH(
 
   return NextResponse.json(updateIssue);
 }
+
+export async function DELETE(
+  request: NextRequest,
+  { params: { id } }: { params: { id: string } }
+) {
+  const issue = await prisma.issue.findUnique({ where: { id: parseInt(id) } });
+  if (!issue)
+    return NextResponse.json({ error: "Issue not found" }, { status: 404 });
+
+  await prisma.issue.delete({
+    where: { id: parseInt(id) },
+  });
+  return NextResponse.json({});
+}
